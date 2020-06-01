2de550728877c659363d2e1ad9b1fdc53fa3c36e
JoanMora <elcorreudejoanmora@gmail.com>
2020-06-01 22:58:36 +0200

    Test Selection Sort.

--- src/sorting/TestElementarySort.java
+++ src/sorting/TestElementarySort.java
@@ -7,12 +7,29 @@ import org.junit.Test;
 
 public class TestElementarySort {
 	
+	public static Integer[] mockArray() {
+		Integer[] array = {99,1,0,5,5,6,98,54,2,7,104};
+		
+		return array;
+	}
+	
 
 	@Test
 	public void testInsertionSort() {
 		
-		Integer[] array = {99,1,0,5,5,6,98,54,2,7,104};
-		ElementarySorting.insertionSort(array);
+		Integer[] array = mockArray();
+ 		ElementarySorting.insertionSort(array);
+		
+		assertTrue( Utils.isSorted(array) );
+		
+		
+	}
+	
+	@Test
+	public void testSelectionSort() {
+		
+		Integer[] array = mockArray();
+		ElementarySorting.selectionSort(array);
 		
 		assertTrue( Utils.isSorted(array) );
 		
