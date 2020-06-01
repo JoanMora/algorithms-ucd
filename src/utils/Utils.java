a2d2f9bd87ca8203abfcd26926f67b4e298ae96d
JoanMora <elcorreudejoanmora@gmail.com>
2020-06-01 20:02:47 +0200

    Shuffle an array of Comparables.

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,6 +1,6 @@
 package utils;
 
-public class Utils {
+public class Utils implements {
 	
 	public static void printArray(Comparable[] array) {
 		for(Comparable e: array) {
@@ -14,6 +14,24 @@ public class Utils {
 		Comparable temp = array[i];
 		array[i] = array[j];
 		array[j] = temp;
+	}
+	
+	public static void shuffle(Comparable[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            // choose index uniformly in [0, i]
+            int r = (int) (Math.random() * (i + 1));
+            Comparable swap = a[r];
+            a[r] = a[i];
+            a[i] = swap;
+        }
+    }
+	
+	public static void main(String[] args) {
+		Integer[] a = {1,2,3,4,5};
+		shuffle(a);
+		
+		printArray(a);
 	}
 
 }
