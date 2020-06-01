82d51fc5297049adc0d41ea0d8bb12bb21ae46cc
JoanMora <elcorreudejoanmora@gmail.com>
2020-06-01 19:53:57 +0200

    Print Array utility.

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,5 +1,13 @@
 package utils;
 
 public class Utils {
+	
+	public static void printArray(Comparable[] array) {
+		for(Comparable e: array) {
+			System.out.print(e);
+			System.out.print(" ");
+		}
+		System.out.println("-------");
+	}
 
 }
